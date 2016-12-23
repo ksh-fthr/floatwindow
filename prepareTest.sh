d0aba07b349404f610c6d61ea3161aa43191486b
Hayato Inoue <ksh.fthr0806@gmail.com>
2016-12-23 16:27:38 +0900

    テスト準備用のスクリプトを実用可能なものに修正

--- prepareTest.sh
+++ prepareTest.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # 単体テストの準備
-# karma init は「karma.conf.js」を git 登録してあるので割愛
+
+CUR_DIR=`pwd`
 
 # npm のインストール
 sudo apt-get install npm
 
 # node が存在しないせいでPATHエラーとなるのでシンボリックリンクを貼る
-/usr/bin//usr/bin/
+cd /usr/bin/
 sudo ln -s nodejs node
 
 # karma のインストール
-cd ${テスト対象のプロジェクトディレクトリ}
+# プロジェクトは 下記にあると想定して動く
+cd $CUR_DIR
 npm install karma
-npm install -g karma-cli
+sudo npm install -g karma-cli
 npm install jasmine-core --save-dev
+
+# karma init を実行
+# 実行後、karma.conf.js を 本ツールを　pull した時に一緒に取得した同名ファイルで置き換える
+cp -p karma.conf.js karma.conf.js.tmp
+karma init
+mv karma.conf.js.tmp karma.conf.js
 
 # DOM操作のテストをするためのプラグインを追加
 npm install karma-html2js-preprocessor --save-dev
