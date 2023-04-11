f9a3df57c65387b2f1d0236caa7e0b4b0a8d1aeb
ksh-fthr <ksh.fthr0806@gmail.com>
2023-04-11 15:08:35 +0900

    tailwidn elements を npm 経由で導入.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   content: [
     './work/**/*.{html,js,ts,jsx,tsx}',
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {}
   },
-  plugins: [
-  ]
+  plugins: [require("tw-elements/dist/plugin")]
 }
 
